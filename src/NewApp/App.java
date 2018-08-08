d68c096b47fb41d3ab3fe948a07b97934148fecb
Edzr-96 <edrz@hotmail.co.uk>
2018-08-08 13:31:32 +0100

    2nd commit

--- src/NewApp/App.java
+++ src/NewApp/App.java
@@ -1,11 +1,23 @@
 package NewApp;
 
+import Feature.Account;
+import Feature.Service;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world to the standard out");
 		
+		Service Feature = new Service();
+		
+		Account BobBobson = new Account("Bob", "Bobson", "1");
+		
+		Account BillBillson = new Account("Bill", "Billson", "2");
 
+		Feature.countServiceFromMap(BobBobson);
+		
+		Feature.countServiceFromMap(BillBillson);
+		
+		System.out.println(Feature.getAccountMap());
 	}
 
 }
